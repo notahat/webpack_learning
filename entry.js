2f3bdd3efa620ef6dc0f317fe07537902d1d567d
Pete Yandell <pete@notahat.com>
2016-01-08 17:12:07 +1100

    Make our JS conform to standard's conventions.

--- entry.js
+++ entry.js
@@ -1,7 +1,7 @@
 import Immutable from 'immutable'
 import installImmutableDevTools from 'immutable-devtools'
 
-document.write("It works.");
+document.write('It really works.')
 
 installImmutableDevTools(Immutable)
-console.log(Immutable.Map({a:1, b:Immutable.List([2,3,4])}))
+console.log(Immutable.Map({a: 1, b: Immutable.List([2, 3, 4])}))
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    entry: "./entry.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: { presets: ['es2015'] }
-            },
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    },
-    devtool: "#source-map"
-};
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: { presets: ['es2015'] }
+      },
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  },
+  devtool: '#source-map'
+}
